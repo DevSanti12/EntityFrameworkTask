cf2ec1c53f639d5c1f3386a1053f81c3e903e6e4
Santiago Leyva <99155189+DevSanti12@users.noreply.github.com>
2025-07-15 10:49:18 -0500

    Fixed comments

--- EntityFrameworkTaskLibrary/DataAccess/ProductContext.cs
+++ EntityFrameworkTaskLibrary/DataAccess/ProductContext.cs
@@ -24,21 +24,9 @@ public class ProductContext: DbContext
                 .Build();
 
             var connectionString = configuration.GetConnectionString("DefaultConnection");
-            // Provide a fallback connection string for design-time tool use
             optionsBuilder.UseSqlServer(connectionString);
         }
     }
-
-    //protected override void OnModelCreating(ModelBuilder modelBuilder)
-    //{
-    //    // Define the relationship between `Order` and `Product`
-    //    modelBuilder.Entity<Order>()
-    //        .HasOne(o => o.ProductId) // Specify that Order has one Product
-    //        .WithMany(p => p.Id)
-    //        .HasForeignKey(o => o.ProductId); // Ensure no duplication
-
-    //    base.OnModelCreating(modelBuilder);
-    //}
 
     public DbSet<Product> Products { get; set; }
     public DbSet<Order> Orders { get; set; }
